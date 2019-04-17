c840092df241a6be335f3d8a677cddc1b82c1818
Jayson Armbruster <Jarmbruster@jerviswebb.com>
2019-04-17 10:57:56 -0400

    Initial commit of angular files and MainComponent

---
+++ BaseballStatsFrontEnd/src/app/Components/MainComponent.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "MainComponent",
+    templateUrl: "../Templates/MainComponent.html"
+})
+
+export class MainComponent {
+    constructor(){
+
+    }
+}
